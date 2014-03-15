8f67fa2b85d99dfe48a8e90da69c82aeed9cd5e3
Wyatt Johnson <wyatt@wyattjohnson.ca>
2014-03-14 20:31:39 -0600

    Fixed build script

--- util/build.sh
+++ util/build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-function dep_files() {
- echo -n "Copy all from /cygdrive/h/NWN/$1/*.$3 -> /cygdrive/c/Games/NeverwinterNights/NWN/$2/"
- cp /cygdrive/h/NWN/$1/*.$3 /cygdrive/c/Games/NeverwinterNights/NWN/$2/
- echo " Done"
-}
-
-function deploy_files() {
-  dep_files $1 $2 $1
-}
-
-
-deploy_files mod modules 
-deploy_files erf erf
-deploy_files hak hak
-dep_files localvault localvault bic
+cp -r ../src/* /cygdrive/c/Games/NeverwinterNights/NWN/
